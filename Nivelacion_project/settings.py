1a1edbe5b4015ae37d25f45e11d264c0226b7c8f
GerardoBedollaR <l21120175@morelia.tecnm.mx>
2025-12-11 14:27:28 -0600

    Update settings.py

--- Nivelacion_project/settings.py
+++ Nivelacion_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(_lqe92b$m&=y)yg(v(p05yl7n&1leve2b*&8bd^snbor($9cv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['repo-gera.onrender.com']
 
 
 # Application definition
